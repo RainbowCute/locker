a80197eb3e545ee5a198b39e61970f3e99025d79
Jie Peng <jie.peng@thoughtworks.com>
2020-09-03 19:59:54 +0800

    feat: return ticket and save bag in 1st locker when robot manage two not full locker and save bag

--- src/main/java/com/thoughtworks/locker/SmartLockerRobot.java
+++ src/main/java/com/thoughtworks/locker/SmartLockerRobot.java
@@ -1,14 +1,22 @@
 package com.thoughtworks.locker;
 
+import com.thoughtworks.locker.exception.FullCapacityException;
+
+import java.util.List;
+
 public class SmartLockerRobot {
 
-    private final Locker locker;
+    private final List<Locker> lockers;
 
-    public SmartLockerRobot(Locker locker) {
-        this.locker = locker;
+    public SmartLockerRobot(List<Locker> lockers) {
+        this.lockers = lockers;
     }
 
     public Ticket save(Bag bag) {
-        return locker.save(bag);
+        return lockers.stream()
+                .filter(Locker::isNotFull)
+                .findFirst()
+                .map(locker -> locker.save(bag))
+                .orElseThrow(FullCapacityException::new);
     }
 }
--- src/test/java/com/thoughtworks/locker/SmartLockerRobotTest.java
+++ src/test/java/com/thoughtworks/locker/SmartLockerRobotTest.java
@@ -2,6 +2,10 @@ package com.thoughtworks.locker;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class SmartLockerRobotTest {
@@ -9,11 +13,24 @@ public class SmartLockerRobotTest {
     @Test
     public void should_return_ticket_when_smart_locker_robot_save_bag_given_a_locker_with_10_capacity_and_bag() {
         Locker locker = new Locker(10);
-        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(locker);
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Collections.singletonList(locker));
         Bag bag = new Bag();
 
         Ticket ticket = smartLockerRobot.save(bag);
 
         assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_return_ticket_and_save_bag_in_1st_locker_when_smart_locker_robot_save_bag_given_1st_locker_with_10_capacity_and_2st_with_5_capacity_and_bag() {
+        Locker firstLocker = new Locker(10);
+        Locker secondLocker = new Locker(5);
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(firstLocker, secondLocker));
+        Bag bag = new Bag();
+
+        Ticket ticket = smartLockerRobot.save(bag);
+
+        assertNotNull(ticket);
+        assertEquals(bag, firstLocker.take(ticket));
     }
 }
