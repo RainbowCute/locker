1996a9be984a0a8a943ac6d0ff9716d10b4d6a9f
zhoumo <mo.zhou@thoughtworks.com>
2020-09-08 20:03:52 +0800

    [zhoumo] #N/A feat: add tasking and return ticket when manager save bag

--- src/test/java/com/thoughtworks/locker/LockerRobotManagerTest.java
+++ src/test/java/com/thoughtworks/locker/LockerRobotManagerTest.java
@@ -3,6 +3,7 @@ package com.thoughtworks.locker;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -14,6 +15,17 @@ public class LockerRobotManagerTest {
         BaseLockerRobot firstRobot = new SmartLockerRobot(Arrays.asList( new Locker(10)));
         BaseLockerRobot secondRobot = new SmartLockerRobot(Arrays.asList( new Locker(10)));
         LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(firstLocker, secondLocker), Arrays.asList(firstRobot, secondRobot));
+        Bag bag = new Bag();
+
+        Ticket ticket = lockerRobotManager.save(bag);
+
+        assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_return_ticket_when_locker_robot_manager_save_bag_given_1_robot_with_free_capacity_and_bag() {
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Collections.singletonList(new Locker(10)));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(null, Collections.singletonList(primaryLockerRobot));
         Bag bag = new Bag();
 
         Ticket ticket = lockerRobotManager.save(bag);
