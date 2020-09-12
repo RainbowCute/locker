0f6ad8e0ab7b42ab087c0bbc28a0867abb918608
zhoumo <mo.zhou@thoughtworks.com>
2020-09-12 17:20:36 +0800

    [zhoumo] #N/A feat: return successful result when manager 1 robot and robot manage 2 locker

--- src/test/java/com/thoughtworks/locker/LockerRobotDirectorTest.java
+++ src/test/java/com/thoughtworks/locker/LockerRobotDirectorTest.java
@@ -8,6 +8,7 @@ import com.thoughtworks.locker.robot.PrimaryLockerRobot;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -49,8 +50,8 @@ public class LockerRobotDirectorTest {
         Locker secondRobotManagedLocker = new Locker(9);
         initFreeCapacity(firstRobotManagedLocker, 5);
         initFreeCapacity(secondRobotManagedLocker, 4);
-        PrimaryLockerRobot firstLockerRobot = new PrimaryLockerRobot(Arrays.asList(firstRobotManagedLocker));
-        PrimaryLockerRobot secondLockerRobot = new PrimaryLockerRobot(Arrays.asList(secondRobotManagedLocker));
+        PrimaryLockerRobot firstLockerRobot = new PrimaryLockerRobot(Collections.singletonList(firstRobotManagedLocker));
+        PrimaryLockerRobot secondLockerRobot = new PrimaryLockerRobot(Collections.singletonList(secondRobotManagedLocker));
         LockerRobotManager lockerRobotManager = new LockerRobotManager(null, Arrays.asList(firstLockerRobot, secondLockerRobot));
         LockerRobotDirector lockerRobotDirector = new LockerRobotDirector();
 
@@ -80,6 +81,39 @@ public class LockerRobotDirectorTest {
         assertEquals(CapacityTitle.L, lockerCapacitiesOfSecondRobot.get(0).getCapacity().getTitle());
         assertEquals(4, lockerCapacitiesOfSecondRobot.get(0).getCapacity().getFreeCapacity());
         assertEquals(9, lockerCapacitiesOfSecondRobot.get(0).getCapacity().getTotalCapacity());
+    }
+
+    @Test
+    public void should_return_successful_result_when_director_count_report_given_locker_robot_manager_mange_one_robot_and_this_robot_mange_two_lockers() {
+        Locker firstLocker = new Locker(8);
+        Locker secondLocker = new Locker(11);
+        initFreeCapacity(firstLocker, 3);
+        initFreeCapacity(secondLocker, 6);
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Arrays.asList(firstLocker, secondLocker));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(null, Collections.singletonList(primaryLockerRobot));
+        LockerRobotDirector lockerRobotDirector = new LockerRobotDirector();
+
+        ManagerCapacityReport managerCapacityReport = lockerRobotDirector.countCapacityReport(lockerRobotManager);
+
+        Capacity managerCapacity = managerCapacityReport.getCapacityItems().get(0).getCapacity();
+        List<CapacityItem> childrenCapacityOfManager = managerCapacityReport.getCapacityItems().get(0).getChildrenCapacity();
+        List<CapacityItem> childrenCapacityOfRobot = managerCapacityReport.getCapacityItems().get(0).getChildrenCapacity().get(0).getChildrenCapacity();
+        assertNotNull(managerCapacityReport);
+        assertEquals(1, managerCapacityReport.getCapacityItems().size());
+        assertEquals(CapacityTitle.M, managerCapacity.getTitle());
+        assertEquals(9, managerCapacity.getFreeCapacity());
+        assertEquals(19, managerCapacity.getTotalCapacity());
+        assertEquals(1, childrenCapacityOfManager.size());
+        assertEquals(CapacityTitle.R, childrenCapacityOfManager.get(0).getCapacity().getTitle());
+        assertEquals(9, childrenCapacityOfManager.get(0).getCapacity().getFreeCapacity());
+        assertEquals(19, childrenCapacityOfManager.get(0).getCapacity().getTotalCapacity());
+        assertEquals(2, childrenCapacityOfRobot.size());
+        assertEquals(CapacityTitle.L, childrenCapacityOfRobot.get(0).getCapacity().getTitle());
+        assertEquals(3, childrenCapacityOfRobot.get(0).getCapacity().getFreeCapacity());
+        assertEquals(8, childrenCapacityOfRobot.get(0).getCapacity().getTotalCapacity());
+        assertEquals(CapacityTitle.L, childrenCapacityOfRobot.get(1).getCapacity().getTitle());
+        assertEquals(6, childrenCapacityOfRobot.get(1).getCapacity().getFreeCapacity());
+        assertEquals(11, childrenCapacityOfRobot.get(1).getCapacity().getTotalCapacity());
     }
 
     private void initFreeCapacity(Locker locker, int freeCapacity) {
